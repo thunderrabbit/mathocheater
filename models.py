a819b709d6126d83237827edcfc5024dcb533c3d
Rob Nugen <rob.git_dreamhost@robnugen.com>
2010-10-28 09:51:36 -0700

    created models.py and README and INSTALL files

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Digits (models.Model):
+    d1 = models.IntegerField()
+    d2 = models.IntegerField()
+    d3 = models.IntegerField()
+    d4 = models.IntegerField()
+    counter = models.IntegerField()
+
+class Answers (models.Model):
+    digits = models.ForeignKey(Digits)
+    solution = models.CharField(max_length=20)
