0ced02debb58ee39074935a88b7db1430a4c28aa
Rob Nugen <rob.git_dreamhost@robnugen.com>
2010-11-01 06:54:35 -0700

    got basic admin working, and updated INSTALL to reflect fixes

---
+++ admin.py
@@ -0,0 +1,5 @@
+from models import Digits, Answers
+from django.contrib import admin
+
+admin.site.register(Digits)
+admin.site.register(Answers)
--- models.py
+++ models.py
@@ -8,7 +8,11 @@ class Digits (models.Model):
     d3 = models.IntegerField()
     d4 = models.IntegerField()
     counter = models.IntegerField()
+    def __unicode__(self):
+        return str(self.d1) + " " + str(self.d2) + " " + str(self.d3) + " " + str(self.d4)
 
 class Answers (models.Model):
     digits = models.ForeignKey(Digits)
     solution = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.solution + " = " + str(eval(self.solution))
