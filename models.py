59428078fe2897098b0d9aee02af10e79af088c3
Rob Nugen <rob.git_dreamhost@robnugen.com>
2010-11-01 12:04:17 -0700

    finished basic version which can solve problems that don't require parenthesis

--- models.py
+++ models.py
@@ -8,11 +8,45 @@ class Digits (models.Model):
     d3 = models.IntegerField()
     d4 = models.IntegerField()
     counter = models.IntegerField()
+
     def __unicode__(self):
         return str(self.d1) + " " + str(self.d2) + " " + str(self.d3) + " " + str(self.d4)
+
+    def solve(self):
+        operators = ['+', '-', '*',  '/']
+
+        operator1 = operator2 = operator3 = ""
+
+        for o1 in operators:
+            for o2 in operators:
+                for o3 in operators:
+                    try:
+                        result = eval(str(self.d1) + o1 + str(self.d2) + o2 + str(self.d3) + o3 + str(self.d4))
+                    except ZeroDivisionError:
+                        result = 0
+                    if(result == 10):
+                        operator1 = o1
+                        operator2 = o2
+                        operator3 = o3
+                        break
+                if(operator3):
+                    break
+            if(operator3):
+                break
+
+        self.save()
+        if(len(operator3)):
+            answer = self.answers_set.create(solution = str(self.d1) + operator1 + str(self.d2) + operator2 + str(self.d3) + operator3 + str(self.d4))
+        else:
+            answer = self.answers_set.create(solution = str(self.d1) + " ? " + str(self.d2) + " ? " + str(self.d3) + " ? " + str(self.d4))
+        return answer
 
 class Answers (models.Model):
     digits = models.ForeignKey(Digits)
     solution = models.CharField(max_length=20)
     def __unicode__(self):
-        return self.solution + " = " + str(eval(self.solution))
+        try:
+            response = self.solution + " = " +  str(eval(self.solution))
+        except:
+            response = "no solution found"
+        return response
--- views.py
+++ views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+from models import Digits, Answers
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the Mathoholic Cheater index.")
+
+def solve(request, d1, d2, d3, d4):
+    answer = Answers.objects.filter(digits__d1__exact=d1,digits__d2__exact=d2,digits__d3__exact=d3,digits__d4__exact=d4)
+    if(answer):
+        output = answer
+    else:
+        digits = Digits(d1 = d1, d2 = d2, d3=d3, d4=d4, counter=0)
+        answer = digits.solve()
+        output = answer
+
+    return HttpResponse(output)
