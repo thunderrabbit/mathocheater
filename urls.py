bcd4b29492be08e26d5b72a5192da0a19aa768b5
Rob Nugen <rob.git_dreamhost@robnugen.com>
2010-11-02 05:56:59 -0700

    rebuilt to use GET instead of tricksy URL parsing also reorganized the view.py logic a bit also changed its apparent name to Mathoholic Solver instead Mathoholic Cheater

--- urls.py
+++ urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mathoholic.mathocheater.views',
     (r'^$', 'index'),
-    (r'^(?P<d1>\d)(?P<d2>\d)(?P<d3>\d)(?P<d4>\d)$', 'solve')
 )
--- views.py
+++ views.py
@@ -4,20 +4,40 @@ from models import Digits, Answers
 import datetime
 
 def index(request):
-    form = DigitsForm()
-    return render_to_response('index.html', {'form':form})
 
-def solve(request, d1, d2, d3, d4):
-    incoming_form = DigitsForm({'digits':d1+d2+d3+d4})
+    four_digits = 0
     empty_form = DigitsForm()
-    if(incoming_form.is_valid()):
-        answer = Answers.objects.filter(digits__d1__exact=d1,digits__d2__exact=d2,digits__d3__exact=d3,digits__d4__exact=d4).values('solution')
-        if(answer):
-            solution = answer[0]['solution']  # thanks to Denis G./M/Volgograd,RussianFederation for this syntax
+    
+    if(request.GET.__contains__('digits')):
+        four_digits = request.GET['digits']
+    
+    if(four_digits):
+        template_name = 'solved.html'
+
+        incoming_form = DigitsForm({'digits':four_digits})
+        if(incoming_form.is_valid() and len(four_digits) == 4):
+            d1 = request.GET['digits'][0];
+            d2 = request.GET['digits'][1];
+            d3 = request.GET['digits'][2];
+            d4 = request.GET['digits'][3];
+
+            answer = Answers.objects.filter(digits__d1__exact=d1,digits__d2__exact=d2,digits__d3__exact=d3,digits__d4__exact=d4).values('solution')
+            if(answer):
+                solution = answer[0]['solution']  # thanks to Denis G./M/Volgograd,RussianFederation for this syntax
+
+            else:
+                digits = Digits(d1 = d1, d2 = d2, d3=d3, d4=d4, counter=0)
+                answer = digits.solve()
+                solution = answer.solution
+
+            if(solution != 'none'):
+                solution = solution + " = " + str(eval(solution))
+            return render_to_response(template_name, {'form':empty_form, 'solution':solution})
+
         else:
-            digits = Digits(d1 = d1, d2 = d2, d3=d3, d4=d4, counter=0)
-            answer = digits.solve()
-            solution = answer.solution
+            return render_to_response(template_name, {'form':incoming_form})
     else:
-        return render_to_response('solved.html', {'form':incoming_form})
-    return render_to_response('solved.html', {'form':empty_form, 'answer':solution})
+        template_name = 'index.html'
+        return render_to_response(template_name, {'form':empty_form})
+
+
