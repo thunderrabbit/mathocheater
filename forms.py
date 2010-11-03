7f5323c4260003299ceedfdb89d45787341e2d25
Rob Nugen <rob.git_MacBook@robnugen.com>
2010-11-03 21:22:06 +0900

    added comments and cleaned up logic in Digits class

--- forms.py
+++ forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class DigitsForm(forms.Form):
-    digits = forms.IntegerField(min_value=0,max_value=9999)
+    """Optimally, this will be a field that requires 4 digits;
+     0019 will work.  0+0+1+9 = 10 but 19 is actually invalid for the game."""
+    digits = forms.IntegerField(min_value=19,max_value=9999)
--- models.py
+++ models.py
@@ -1,14 +1,18 @@
 from django.db import models
 import re
 
-# Create your models here.
-
 class Digits (models.Model):
+    """Basically stores 4 digits and knows how to run through permutations of operators 
+    (including parenthesis) to figure out a combination that equals 10.
+
+    Eventually will increment the counter each time a set of 4 digits is processed, but that's not
+    written yet until I set up a table to block repeats by the same IP address in short time span."""
+
     d1 = models.IntegerField()
     d2 = models.IntegerField()
     d3 = models.IntegerField()
     d4 = models.IntegerField()
-    counter = models.IntegerField()  # deprecated
+    counter = models.IntegerField()  # reserved for future use
 
     def __unicode__(self):
         return str(self.d1) + " " + str(self.d2) + " " + str(self.d3) + " " + str(self.d4)
@@ -24,19 +28,33 @@ class Digits (models.Model):
         result_string = ""
         result_float_string = ""
         result_int = 0
+
+        # force floating point math by adding ".0" to end of each digit
+        d1 = str(self.d1) + ".0"
+        d2 = str(self.d2) + ".0"
+        d3 = str(self.d3) + ".0"
+        d4 = str(self.d4) + ".0"
+
+        #-------------------------------------------------------------------#
+        #
+        #  this begins a brute force process of evaluating all the possible
+        #  permutations of equations until it finds one that equals 10, or
+        #  finds none equal 10.
+        #
+        #------------------------------------------------------------------#
         for parenthesis_option in parenthesis_options:
             self.set_parenthesis(parenthesis_option)
             for o1 in operators:
                 for o2 in operators:
                     for o3 in operators:
-                        result_string = self.paren1 + str(self.d1) + o1 + self.paren2 + str(self.d2) + self.paren3 + o2 + self.paren4 + str(self.d3) + self.paren5 + o3 + str(self.d4) + self.paren6
-                        # force floating point math by adding ".0" to end of each digit
-                        result_float_string = re.sub(r'(\d)',r'\1.0',result_string)
+                        result_float_string = self.paren1 + d1 + o1 + self.paren2 + d2 + self.paren3 + o2 + self.paren4 + d3 + self.paren5 + o3 + d4 + self.paren6
                         try:
                             result_int = eval(result_float_string)
                         except ZeroDivisionError:
                             result_int = 0
                         if(result_int == 10):
+                            # remove ".0" from each number to make them whole digits
+                            result_string = re.sub(r'\.0','',result_float_string)
                             finished = 1
                             break
                     if(finished):
@@ -46,6 +64,13 @@ class Digits (models.Model):
             if(finished):
                 break
 
+        #------------------------------------------------------------#
+        #
+        #  here we need to see if the counter needs to be updated
+        #  I'm thinking of it's same IP address during same hour,
+        #  do *not* count it.
+        #  
+        #------------------------------------------------------------#
         self.save()
         if(finished):
             answer = self.answers_set.create(solution = result_string)
@@ -58,28 +83,34 @@ class Digits (models.Model):
         if(p_option == 'none'):
             pass
         elif(p_option == 'first pair'):
+            # (A + B) + C + D
             self.paren1 = '('
             self.paren3 = ')'
         elif(p_option == 'second pair'):
+            # A + (B + C) + D
             self.paren2 = '('
             self.paren5 = ')'
         elif(p_option == 'third pair'):
+            # A + B + (C + D)
             self.paren4 = '('
             self.paren6 = ')'
         elif(p_option == 'both pairs'):
+            # (A + B) + (C + D)
             self.paren1 = '('
             self.paren3 = ')'
             self.paren4 = '('
             self.paren6 = ')'
         elif(p_option == 'first three'):
+            # (A + B + C) + D
             self.paren1 = '('
             self.paren5 = ')'
         elif(p_option == 'last three'):
+            # A + (B + C + D)
             self.paren2 = '('
             self.paren6 = ')'
 
-            
 class Answers (models.Model):
+    ''' stores an answer for a set of digits or 'none' if none exists '''
     digits = models.ForeignKey(Digits)
     solution = models.CharField(max_length=20)
     def __unicode__(self):
